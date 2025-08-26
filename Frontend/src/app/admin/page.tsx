e62070533a4728581d0d35388b2c4b08c0fff5ed
Lovedeep <lovedeep24253@gmail.com>
2025-08-26 18:24:24 +0530

    fix:Fixed deployment error

--- Frontend/src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AdminHome } from '@/components/AdminHome'
-export default function page() {
-  return (
-    <div>
-      <AdminHome />
-    </div>
-  )
-}
--- Frontend/src/components/AdminHome.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AdminHome() {
-  return (
-    <div>
-      <h1>Just a admin page</h1>
-    </div>
-  )
-}
